ac000/encode_vobs

commit b141fed679fbeb9a27d3cab1c0f4e00353b922ab

Add the ability to run a program after each file encoding
Add an optional -e argument that takes a full path to a program/script
that will called with the name of the newly encoded file as argv[1]
after each file is encoded.

This would be useful if some post processing of the file is required.

Signed-off-by: Andrew Clayton <andrew@digital-domain.net>

encode_vobs.c
@@ -32,15 +32,17 @@
 			tm->tm_hour, tm->tm_min, tm->tm_sec, ##__VA_ARGS__); \
 	} while (0)
 
-#define NR_WORKERS	nr_workers
 #define PROCESS_EXITED	-2
 
+#define NR_WORKERS	nr_workers
 #define ENC_NICE	enc_nice
+#define POST_CMD	post_cmd
 
 static int enc_nice = 10;
 static int files_to_process;
 static int files_processed;
 static int nr_workers;
+static char *post_cmd;
 
 struct processing {
 	pid_t pid;
@@ -90,10 +92,17 @@ static void create_mkv(const char *infile, const char *outfile)
 static void disp_usage(void)
 {
 	fprintf(stderr, "Usage: encode_vobs -P <theora|webm|mkv> [-t tasks] "
-			"[-n nice] file1 ...\n\n");
-	fprintf(stderr, "nice is the priority to run the encode processes "
-			"at.\nIt should be a value between 0 and 19. It "
-			"defaults to 10.\n");
+			"[-n nice]\n       [-e post_process_exec_command] "
+			"<file1 ...>\n\n");
+	fprintf(stderr, "tasks is how many files to process at a time. It "
+			"defaults to nr cores - 1.\n\n"
+			"nice is the priority to run the encode processes "
+			"at. It should be a value\nbetween 0 and 19. It "
+			"defaults to 10.\n\n"
+			"post_process_exec_command is the full path to an "
+			"executable that will be\ncalled after each processed "
+			"file, it will be passed the full path of the\n"
+			"newly processed file as argv[1].\n");
 	exit(EXIT_FAILURE);
 }
 
@@ -113,6 +122,15 @@ static void reaper(int signo)
 	}
 }
 
+static void do_post_cmd(const char *file)
+{
+	pid_t pid;
+
+	pid = fork();
+	if (pid == 0)
+		execl(POST_CMD, POST_CMD, file, (char *)NULL);
+}
+
 static void do_processed(void)
 {
 	int i;
@@ -120,6 +138,9 @@ static void do_processed(void)
 	for (i = 0; i < NR_WORKERS; i++) {
 		if (processing[i].pid == PROCESS_EXITED) {
 			loginfo("Finished   : %s\n", processing[i].file);
+			if (POST_CMD)
+				do_post_cmd(processing[i].file);
+
 			processing[i].pid = -1;
 			processing[i].file[0] = '\0';
 			files_processed++;
@@ -183,8 +204,24 @@ int main(int argc, char **argv)
 	struct sigaction sa;
 	const char *profile = '\0';
 
-	while ((opt = getopt(argc, argv, "P:hn:t:")) != -1) {
+	while ((opt = getopt(argc, argv, "e:P:hn:t:")) != -1) {
 		switch (opt) {
+		case 'e': {
+			struct stat st;
+			int err;
+
+			if (!optarg)
+				disp_usage();
+
+			err = stat(optarg, &st);
+			if (!err) {
+				post_cmd = optarg;
+			} else {
+				fprintf(stderr, "Cannot stat %s\n", optarg);
+				exit(EXIT_FAILURE);
+			}
+			break;
+		}
 		case 'P':
 			if (strcmp(optarg, "theora") != 0 &&
 			    strcmp(optarg, "webm") != 0 &&

README
@@ -8,7 +8,7 @@ It creates nr cores - 1 (over-ridable with the -t option),
 ffmpeg2theora/ffmpeg processes at a time, running them at nice +10
 (over-ridable with the -n option).
 
-Usage is like:
+Usage is like (pass -h for full help):
 
     $ cd /path/to/vobs
     $ encode_vobs -P webm file1.vob ...
@@ -19,5 +19,10 @@ for Matroska.
 When using the mkv profile, it expects a VOB containing the video as the
 first track and the audio you want as the second (extra audio tracks will be
 ignored).
+
+You can also optionally pass the full path to an executable that you would
+like to have run after every file is encoded with the -e option. This can be
+a binary or script of some sort, it will be called with the newly encoded
+file name as argv[1].
 
 This project is licensed under the GNU GPLv2.

commit 9c9080146765f02aa09edd171a17f5198718c055

Fix some process management issues
There were two main problems observed.

    1) If using a post process script/program you could end up with more
       encode processes than NR_WORKERS. This problem was introduced by
       commit b141fed ("Add the ability to run a program after each file
       encoding"). The issue is that when the external program exits
       encode_vobs will receive a SIGCHLD which will end up with another
       encode process being run regardless.

    2) Sometimes do_processed() would not get run which would mean no
       "Finished" message and no post encode process being run. This
       problem was likely always there it was just much more likely to
       be hit when running post processes due to some signal handling
       race condition.

Fix these issues by re-working the main process loop greatly simplifying
it in the process. The race condition in no 2 may still be there but it
is greatly reduced and I haven't been able to hit the problem with this
commit but it is 100% reproducible without.

Signed-off-by: Andrew Clayton <andrew@digital-domain.net>

encode_vobs.c
@@ -38,9 +38,11 @@
 #define ENC_NICE	enc_nice
 #define POST_CMD	post_cmd
 
+static volatile sig_atomic_t files_in_progress;
+static volatile sig_atomic_t files_processed;
+static volatile sig_atomic_t file_processed;
+
 static int enc_nice = 10;
-static int files_to_process;
-static int files_processed;
 static int nr_workers;
 static char *post_cmd;
 
@@ -116,6 +118,9 @@ static void reaper(int signo)
 		for (i = 0; i < NR_WORKERS; i++) {
 			if (processing[i].pid == pid) {
 				processing[i].pid = PROCESS_EXITED;
+				files_in_progress--;
+				files_processed++;
+				file_processed = 1;
 				break;
 			}
 		}
@@ -143,9 +148,9 @@ static void do_processed(void)
 
 			processing[i].pid = -1;
 			processing[i].file[0] = '\0';
-			files_processed++;
 		}
 	}
+	file_processed = 0;
 }
 
 static void process_file(const char *file, const char *profile)
@@ -200,7 +205,7 @@ int main(int argc, char **argv)
 {
 	int i;
 	int opt;
-	int files_in_progress = 0;
+	int files_to_process;
 	struct sigaction sa;
 	const char *profile = '\0';
 
@@ -266,21 +271,16 @@ int main(int argc, char **argv)
 
 	files_to_process = argc - optind;
 	i = optind;
-	for (;;) {
-		if (i == argc) {
-			while (files_processed < files_to_process) {
-				pause();
-				do_processed();
-			}
-			break;
-		} else if (files_in_progress == NR_WORKERS) {
+	while (files_processed < files_to_process) {
+		if (files_in_progress < NR_WORKERS && i < argc) {
+			process_file(argv[i], profile);
+			files_in_progress++;
+			i++;
+		} else {
 			pause();
-			files_in_progress--;
-			do_processed();
 		}
-		process_file(argv[i], profile);
-		i++;
-		files_in_progress++;
+		if (file_processed)
+			do_processed();
 	}
 
 	free(processing);