ae7caa6d69b4c5d0714f679ac8111ceddb7ed632
Andrew Clayton <andrew@digital-domain.net>
2018-06-23 21:13:37 +0100

    Update the copyright years
    Signed-off-by: Andrew Clayton <andrew@digital-domain.net>

--- encode_vobs.c
+++ encode_vobs.c
@@ -1,7 +1,7 @@
 /*
  * encode_vobs.c - Simple job scheduler to encode vob dvd rips
  *
- * Copyright (C) 	2012 - 2015, 2017
+ * Copyright (C) 	2012 - 2015, 2017 - 2018
  * 			Andrew Clayton <andrew@digital-domain.net>
  *
  * Released under the GNU General Public License version 2
